f44b8c79ab758b9a3bdb68c9d79ea4ceba774cbd
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-09-20 06:10:40 +0100

    revert(vivado): Revert build.tcl script back to non-project mode

--- py2hwsw/hardware/fpga/vivado/build.tcl
+++ py2hwsw/hardware/fpga/vivado/build.tcl
@@ -9,79 +9,81 @@ foreach var $vars arg $argv {
     puts "$var = $arg"
 }
 
-set proj_dir "./vivado_proj"
-set proj_name "${NAME}_prj"
-
-# Create project
-create_project $proj_name $proj_dir -force
-
-# Add Verilog and EDIF sources
+# Read verilog sources, vivado IPs, use file extension
 foreach file [split $VSRC \ ] {
     puts $file
-    if {[file extension $file] == ".edif"} {
-        add_files -fileset sources_1 $file
+    if { [ file extension $file ] == ".edif" } {
+        read_edif $file
     } elseif {$file != "" && $file != " " && $file != "\n"} {
-        add_files -fileset sources_1 $file
+        read_verilog -sv $file
     }
 }
 
-# Set the include directories property on the 'sources_1' fileset
-# Vivado expects one string with space-separated directories
-set_property include_dirs [join $INCLUDE_DIRS " "] [get_filesets sources_1]
+# include directories, use -include_dirs option
+set SYNTH_FLAGS {}
+foreach dir $INCLUDE_DIRS {
+    lappend SYNTH_FLAGS "-include_dirs" "${dir}"
+}
 
 # Read board properties
 source vivado/$BOARD/board.tcl
 
-# Pre-map custom assignments
+
+# Set pre-map custom assignments
 if {[file exists "vivado/premap.tcl"]} {
     source "vivado/premap.tcl"
 }
 
-# Add SDC constraints
-if {[file exists "vivado/$BOARD/$NAME\_dev.sdc"]} {
-    add_files -fileset constrs_1 vivado/$BOARD/$NAME\_dev.sdc
-}
-if {[file exists "../src/$NAME.sdc"]} {
-    add_files -fileset constrs_1 ../src/$NAME.sdc
-}
-if {[file exists "../../src/$NAME\_$CSR_IF.sdc"]} {
-    add_files -fileset constrs_1 ../src/$NAME\_$CSR_IF.sdc
-}
-if {[file exists "vivado/$NAME\_tool.sdc"]} {
-    add_files -fileset constrs_1 vivado/$NAME\_tool.sdc
-}
 
-# Set top module
-set_property top $FPGA_TOP [current_fileset]
-
-# Out-of-context/FPGA flag handling (optional, for simple project mode we skip OOC)
-# You can add support for OOC runs if needed
-
-# Run Synthesis
-launch_runs synth_1
-wait_on_run synth_1
-
-# Open synthesized design
-open_run synth_1
+# Read design constraints and synthesize design
+if { $IS_FPGA == "1" } {
+    puts "Synthesizing for FPGA"
+    if {[file exists "vivado/$BOARD/$NAME\_dev.sdc"]} {
+        read_xdc vivado/$BOARD/$NAME\_dev.sdc
+    }
+    if {[file exists "../src/$NAME.sdc"]} {
+        read_xdc ../src/$NAME.sdc
+    }
+    if {[file exists "../../src/$NAME\_$CSR_IF.sdc"]} {
+        read_xdc ../src/$NAME\_$CSR_IF.sdc
+    }
+    if {[file exists "vivado/$NAME\_tool.sdc"]} {
+        read_xdc vivado/$NAME\_tool.sdc
+    }
+    eval synth_design -include_dirs ../src -include_dirs ../common_src -include_dirs ./src -include_dirs ./vivado/$BOARD $SYNTH_FLAGS -part $PART -top $FPGA_TOP -verbose
+} else {
+    # Read design constraints
+    puts "Out of context synthesis"
+    read_xdc -mode out_of_context vivado/$BOARD/$NAME\_dev.sdc
+    read_xdc -mode out_of_context ../src/$NAME.sdc
+    if {[file exists "../src/$NAME\_$CSR_IF.sdc"]} {
+        read_xdc ../src/$NAME\_$CSR_IF.sdc
+    }
+    if {[file exists "./src/$NAME.sdc"]} {
+        read_xdc ./src/$NAME.sdc
+    }
+    if {[file exists "vivado/$NAME\_tool.sdc"]} {
+        read_xdc -mode out_of_context vivado/$NAME\_tool.sdc
+    }
+    eval synth_design -include_dirs ../src -include_dirs ../common_src -include_dirs ./src -include_dirs ./vivado/$BOARD $SYNTH_FLAGS -part $PART -top $FPGA_TOP -mode out_of_context -flatten_hierarchy full -verbose
+}
 
 # Apply constraints via .tcl script (allows conditional constraints)
 if {[file exists "vivado/$BOARD/$NAME\_dev.tcl"]} {
     source vivado/$BOARD/$NAME\_dev.tcl
 }
 
-# Post-map custom assignments
+# Set post-map custom assignments
 if {[file exists "vivado/postmap.tcl"]} {
     source "vivado/postmap.tcl"
 }
 
-# Run Implementation
-launch_runs impl_1
-wait_on_run impl_1
+opt_design
 
-# Open implemented design
-open_run impl_1
+place_design
 
-# Reports (after implementation)
+route_design -timing
+
 report_clocks
 report_clock_interaction
 report_cdc -details
@@ -97,17 +99,9 @@ report_timing_summary -file reports/$FPGA_TOP\_$PART\_timing_summary.rpt
 report_timing -file reports/$FPGA_TOP\_$PART\_timing_paths.rpt -max_paths 30
 report_bus_skew -file reports/$FPGA_TOP\_$PART\_bus_skew.rpt
 
-if {$IS_FPGA == "1"} {
+if { $IS_FPGA == "1" } {
     write_bitstream -force $FPGA_TOP.bit
-    write_hw_platform -fixed -include_bit -force hw_platform.xsa
 } else {
-    # Non-FPGA flow, optional: export netlist
     write_verilog -force $FPGA_TOP\_netlist.v
     write_verilog -force -mode synth_stub ${FPGA_TOP}_stub.v
 }
-
-# FIXME: Apparently this does not work for vivado projects that do not have a block design.
-# # Export hardware platform (used to later import in Xilinx vitis for FSBL generation on Zynq boards)
-# set_property platform.board_id $BOARD [current_project]
-# set_property platform.name $NAME [current_project]
-# write_hw_platform -force -file hw_platform.xsa
